03b1cb3164c95e9a35c0a5041432160b150ca086
Minseung <mskangsky@snu.ac.kr>
2024-05-22 17:07:37 +0900

    aihwkit_demos

--- src/MNIST_MLP/debug.py
+++ src/MNIST_MLP/debug.py
@@ -1,27 +1,17 @@
-import os
-from time import strftime, localtime
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-import torch
-import torch.utils.data
-from torch.utils.data import DataLoader
-import torchvision.datasets as dsets
-from torchvision.transforms import ToTensor
-from torch import Tensor
-
-from network import MNIST_MLP
-from visualize import vis_model
-from device_loader import DeviceDataLoader
-from device_loader import to_device
-
-import aihwkit
-from aihwkit.nn import AnalogLinear
 from aihwkit.simulator.configs import InferenceRPUConfig
-from aihwkit.simulator.configs.devices import OneSidedUnitCell
 
-"""==================================================="""
-rpu_config      = InferenceRPUConfig()
-rpu_config.device = OneSidedUnitCell()
-print(rpu_config.device)
+# Define the analog model, e.g., by converting a given floating point (FP) model
+from torchvision.models import resnet34  # e.g., trained model from torch
+
+rpu_config  = InferenceRPUConfig()
+
+analog_model = convert_to_analog(resnet34(), rpu_config, weight_scaling_omega=1.0)
+
+# [... do hardware-aware (re)-training ...]
+
+# Evaluate the model after programming the weights and driting over a certain period of time
+analog_model = AnalogSequential(analog_model).eval()
+t_inference = 1000 #desired time of drift in seconds after programming
+analog_model.drift_analog_weights(t_inference)
+
+# [... evaluate programmed drifted model ...]
