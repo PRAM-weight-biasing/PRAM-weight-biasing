a424b5af44aa3a1d53399346f29548f70be894a3
seyoon <yoony1631@gmail.com>
2024-10-18 15:46:16 +0900

    pruning test

--- tests/prune_weights.py
+++ tests/prune_weights.py
@@ -7,28 +7,21 @@ import myModule
 from network import PruneModel, TrainModel, Vis_Model
 from Model.PyTorch_CIFAR10.cifar10_models.resnet import resnet18
 
-### =============================================================
-
+# =============================================================
 
 # Import model
 model_type = input("Input model type? (MLP/Resnet18) : ")
-prune_percentage = input('Prune percentage? (0~1) :')
+prune_percentage = float(input('Pruning percentage? (0~1) : '))
 
 if model_type == 'MLP':
-    dir_name = os.getcwd() + '/TestRun/'
-    # ===========================================
-    """ need to change """
-    test_time = "Test_2024-08-01 11:52:18" 
-    # ===========================================
-    folder_path = dir_name + test_time
-    model_name = 'best_model.pth'
+    dir_name = os.getcwd() + '/Model/MLP'
+    model_name = torch.load(f'{dir_name}/best_model.pth')
     
 elif model_type == 'Resnet18':
     model_name = resnet18(pretrained=True)
-    folder_path = myModule.MakeFolder(f'_prune{prune_percentage}')
+    
+folder_path = myModule.MakeFolder(f'_{model_type}_p{prune_percentage}')
 
-
-# prune_percentage = 0.6
 
 # local pruning : prune each layer
 PruneTest1 = PruneModel(model_name, folder_path)
@@ -39,7 +32,7 @@ PruneTest1.Vis_local_w()
 PruneTest1.Vis_global_w()
 
 # global pruning : prune the whole network
-PruneTest2 = PruneModel(prune_percentage, model_name, folder_path)
+PruneTest2 = PruneModel(model_name, folder_path)
 PruneTest2.apply_global_pruning(prune_percentage)
 sparsity_list2 = PruneTest2.cal_local_sparsity('global_pruned_model.pth')
 sparsity_all2 = PruneTest2.cal_global_sparsity('global_pruned_model.pth')
