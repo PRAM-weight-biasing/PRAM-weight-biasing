af37de426dd3333fc08fa555d413a30aaaf5e22f
seyoon <yoony1631@gmail.com>
2024-12-11 15:47:27 +0900

    Test_mlp-retrain-inf

--- tests/inference_drift.py
+++ tests/inference_drift.py
@@ -28,11 +28,11 @@ dir_name = os.getcwd() + '/TestRun/'
 # test_time = "Test_2024-08-01 11:52:18" 
 # folder_name = test_time
 
-name_list = ["Test_2024-10-18_13-45_MLP_p0.3",
-             "Test_2024-10-24_13-35_MLP_p0.4",
-             "Test_2024-10-24_13-36_MLP_p0.5",
-             "Test_2024-10-24_13-36_MLP_p0.6",
-             "Test_2024-10-24_13-36_MLP_p0.7"
+name_list = ["Test_2024-10-18_13-45_MLP_p0.3"
+             , "Test_2024-10-24_13-35_MLP_p0.4"
+             , "Test_2024-10-24_13-36_MLP_p0.5"
+             , "Test_2024-10-24_13-36_MLP_p0.6"
+             , "Test_2024-10-24_13-36_MLP_p0.7"
                ]
 # ===========================================
 
@@ -77,7 +77,7 @@ for folder_name in name_list:
     print(f'folder : {folder_name}')
     
     folder_path = dir_name + folder_name
-    model_name = 'retrain/best_model.pth'
+    model_name = 'local_pruned_model.pth'
 
     model = torch.load(f'{folder_path}/{model_name}')
 
@@ -94,7 +94,8 @@ for folder_name in name_list:
     analog_model = inf_model.ConvertModel()  # convert s/w model to analog h/w model using aihwkit
 
     # Inference
-    t_inferences = [0.0, 10.0, 100.0, 1000.0, 3600.0, 10000.0, 86400.0, 1e7, 1e8, 1e9, 1e10, 1e12, 1e15]
+    # t_inferences = [0.0, 10.0, 100.0, 1000.0, 3600.0, 10000.0, 86400.0, 1e7, 1e8, 1e9, 1e10, 1e12, 1e15]
+    t_inferences = [0.0, 10.0, 100.0, 1000.0, 3600.0, 10000.0, 86400.0, 1e7, 1e8, 1e9]
     n_reps = 10   # Number of inference repetitions.
 
     inf_model.hw_EvalModel(analog_model, testloader, t_inferences, n_reps)
